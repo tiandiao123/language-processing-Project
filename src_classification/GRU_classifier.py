61ae797f1d030100058bc17004e9ccf667ad9a84
Cuiqing Li <Cuiqing Li>
2017-05-03 02:20:36 -0400

    add new file

---
+++ src_classification/GRU_classifier.py
@@ -0,0 +1,9 @@
+import numpy
+import matplotlib.pyplot as plt
+from pandas import read_csv
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
