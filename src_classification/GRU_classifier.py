081455a4a8e49a143f4cbbc02e28aba5f2223856
Cuiqing Li <lixx3527@umn.edu>
2017-05-03 23:06:14 -0400

    working on GRU

--- src_classification/GRU_classifier.py
+++ src_classification/GRU_classifier.py
@@ -1,9 +1,13 @@
 import numpy
 import matplotlib.pyplot as plt
-from pandas import read_csv
 import math
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+from process_data import handle_flu_json,handle_flu_risk_perception,handle_flu_vaccine_new
+from process_data import handle_health_json,handle_trust_in_gov,handle_vaccine_sentiment,handle_zika_conspiracy
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from Tokenizer_text import tokenize_words
--- src_classification/Tokenizer_text.py
+++ src_classification/Tokenizer_text.py
@@ -9,17 +9,33 @@ import gzip
 from process_data import handle_flu_json,handle_flu_risk_perception,handle_flu_vaccine_new
 from process_data import handle_health_json,handle_trust_in_gov,handle_vaccine_sentiment,handle_zika_conspiracy
 import nltk
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
 
 vocabulary_size=8000
 unknown_token = "UNKNOWN_TOKEN"
 sentence_start_token="SENTENCE_START_TOKEN"
 
-txt_flu,labels_flu=handle_flu_json("../data/flu.json.gz")
-sentences=[]
-for i in range(len(txt_flu)):
-	sentences.append("{} {} {}".format(sentence_start_token,txt_flu[i],labels_flu[i]))
 
-sentences=np.array(sentences)
-for ele in sentences:
-	pprint(ele)
-	time.sleep(1)
+
+txt_flu,labels_flu=handle_flu_json("../data/flu.json.gz")
+
+def tokenize_words(txt,labels):
+	sentences=[]
+	for i in range(len(txt)):
+		sentences.append("{} {} {}".format(sentence_start_token,txt[i],labels[i]))
+	tokenizer = Tokenizer()
+	tokenizer.fit_on_texts(sentences)
+	sequences = tokenizer.texts_to_sequences(sentences)
+	label_index=tokenizer.texts_to_sequences(labels)
+	return sequences,label_index
+
+
+
+# sequences,label_index=token_words(txt_flu,labels_flu)
+
+# for i in range(len(sequences)):
+# 	pprint(sequences[i])
+# 	time.sleep(2)
+# 	pprint(label_index[i])
+# 	time.sleep(1)
