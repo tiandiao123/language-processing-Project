3099cfea15ac0aa4175ba1cfc54700cac133dea7
Cuiqing Li <lixx3527@umn.edu>
2017-05-06 02:41:37 -0400

    change GRU model

--- src_classification/GRU_classifier.py
+++ src_classification/GRU_classifier.py
@@ -3,6 +3,7 @@ from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM,GRU
+from keras.layers import Dropout
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 from pprint import pprint
@@ -13,15 +14,22 @@ from keras.preprocessing.text import Tokenizer
 from Tokenizer_text import tokenize_words
 import time
 import Tokenizer_text
+from sklearn.metrics import recall_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import GridSearchCV, cross_val_score
 
 
-def GRU_train_prediction(sequences,labels,max_review_length):
+def GRU_train_prediction(sequences,labels,max_review_length,top_words):
 	#split the data into training data and test data
-	top_words = 10000
+	#top_words = 10000
 	labels=np.array(labels)
 	label_array=labels.flatten()
 
 	label2index=list(set(label_array))
+	#print(label2index)
 	prediction_labels=[]
 	
 	for i in range(len(labels)):
@@ -29,6 +37,7 @@ def GRU_train_prediction(sequences,labels,max_review_length):
 			prediction_labels.append(0)
 		else:
 			prediction_labels.append(1)
+	#print(prediction_labels)
 
 	prediction_labels=np.array(prediction_labels)
 	X_train,X_test,y_train,y_test=train_test_split(sequences,prediction_labels,test_size=0.33)
@@ -43,19 +52,43 @@ def GRU_train_prediction(sequences,labels,max_review_length):
 	embedding_vecor_length = 32
 	model = Sequential()
 	model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
-	model.add(GRU(50))
+	model.add(Dropout(0.2))
+	model.add(GRU(30))
+	model.add(Dropout(0.2))
 	model.add(Dense(1, activation='sigmoid'))
 	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 	print(model.summary())
-	model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
+	model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=2, batch_size=64)
 	scores = model.evaluate(X_test, y_test, verbose=0)
-	print("Accuracy: %.2f%%" % (scores[1]*100))
+	
+	probs = model.predict(x=X_test)
+	predics=[]
+	for ele in probs:
+		if ele>=0.5:
+			predics.append(1)
+		else:
+			predics.append(0)
+
+	Recall=recall_score(y_test, predics, average='macro') 
+	Precision= precision_score(y_test, predics, average='macro')
+	F_1=2*(Recall*Precision)/(Precision+Recall)
+	auc=roc_auc_score(y_test,probs)
+
+	print("Testing Accuracy: %.2f%%" % (scores[1]*100))
+	print("F_1 for testing data is : {}".format(str(F_1)))
+	print("Recall for testing data is : {}".format(str(Recall)))
+	print("Precision for testing data is : {}".format(str(Precision)))
+	print("the auc value for testing data is :{}".format(str(auc)))
 	
 
-top_words = 6000
+
 txt_flu,labels_flu=handle_flu_json("../data/flu.json.gz")
 sequences,labels=tokenize_words(txt_flu,labels_flu)
-GRU_train_prediction(sequences,labels,40)
+GRU_train_prediction(sequences,labels,40,10000)
+
+
+
+##################################################################################################################
 
 #split the data into training data and test data
 # top_words = 5000
@@ -92,31 +125,5 @@ GRU_train_prediction(sequences,labels,40)
 
 ###################################################################################################
 
-# top_words = 5000
-# (X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
-# max_review_length = 500
-# X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
-# X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
-
-# print(X_train.shape)
-
-# for ele in X_train:
-# 	print(type(ele))
-# 	time.sleep(1)
-
-# create the model
-# embedding_vecor_length = 32
-# model = Sequential()
-# model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
-# #model.add(Dropout(0.2))
-# model.add(LSTM(100))
-# #model.add(Dropout(0.2))
-# model.add(Dense(1, activation='sigmoid'))
-# model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-# print(model.summary())
-# model.fit(X_train, y_train, epochs=3, batch_size=64)
-# # Final evaluation of the model
-# scores = model.evaluate(X_test, y_test, verbose=0)
-# print("Accuracy: %.2f%%" % (scores[1]*100))
 
 
